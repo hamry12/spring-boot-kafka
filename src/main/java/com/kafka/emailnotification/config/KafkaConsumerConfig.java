b46b7dcc1532a586b30407e3c1e48321953202e4
Himanshu <h_himanshu2007@yahoo.com>
2025-01-31 09:30:53 +0530

    Kafka: Consumer Microservice configuration update

---
+++ src/main/java/com/kafka/emailnotification/config/KafkaConsumerConfig.java
@@ -0,0 +1,45 @@
+package com.kafka.emailnotification.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+
+    /**
+     * Instead of Using @Value; Direct Environment variables can be used
+     * to load the data to the beans
+     */
+
+    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    private String CONSUMER_BOOTSTRAP_SERVER;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String CONSUMER_GROUP_ID;
+
+    @Value("${spring.kafka.consumer.properties.spring.json.trusted.packages}")
+    private String JSON_TRUSTED_PACKAGES;
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory(){
+        Map<String, Object> config= new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, CONSUMER_BOOTSTRAP_SERVER);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, CONSUMER_GROUP_ID);
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, JSON_TRUSTED_PACKAGES);
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+
+}
