864571eb381a5f74d12f4c35549a467fc0e54e1c
Himanshu <h_himanshu2007@yahoo.com>
2025-01-31 09:50:51 +0530

    kafka: corrected consumer configuration to receive the events

--- src/main/java/com/kafka/emailnotification/config/KafkaConsumerConfig.java
+++ src/main/java/com/kafka/emailnotification/config/KafkaConsumerConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
@@ -39,6 +40,16 @@ public class KafkaConsumerConfig {
         config.put(ConsumerConfig.GROUP_ID_CONFIG, CONSUMER_GROUP_ID);
         config.put(JsonDeserializer.TRUSTED_PACKAGES, JSON_TRUSTED_PACKAGES);
         return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory
+    ){
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory=
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
     }
 
 
